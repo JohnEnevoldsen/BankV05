81f173f9059e85d0af57bc4741f508ecf85af9fb
JohnEnevoldsen <62882814+JohnEnevoldsen@users.noreply.github.com>
2020-03-30 13:32:10 +0200

    Initial commit

---
+++ BankAccount.cs
@@ -0,0 +1,36 @@
+﻿namespace BankV05
+{
+    /// <summary>
+    /// This class represents a very simple bank account.
+    /// Only the amount of money on the account is represented.
+    /// </summary>
+    public class BankAccount
+    {
+        private double _balance;
+        private string _owner;
+
+        public BankAccount(string owner)
+        {
+            _balance = 0.0;
+            _owner = owner;
+        }
+
+        public double Balance
+        {
+            get { return _balance;}
+        }
+
+        public void Deposit(double amount)
+        {
+            _balance = _balance + amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            _balance = _balance - amount;
+        }
+        public string Owner{
+            get {return _owner;}
+        }
+    }
+}
---
+++ InsertCodeHere.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace BankV05
+{
+    public class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+
+            BankAccount myAccount = new BankAccount("John");
+            BankAccount glebAccount = new BankAccount("Gleb");
+            BankAccount simonAccount = new BankAccount("Simon");
+
+            simonAccount.Deposit(1000);
+            myAccount.Deposit(2000);
+            Console.WriteLine($"Account balance is : {myAccount.Balance}");
+
+            simonAccount.Deposit(1000);
+            myAccount.Withdraw(1500);
+            Console.WriteLine($"Account balance is : {myAccount.Balance}");
+            Console.WriteLine($"The owner of this account is : {myAccount.Owner}");
+
+            simonAccount.Deposit(1000);
+            glebAccount.Deposit(9000);
+            Console.WriteLine($"{glebAccount.Owner}'s Account balance is : {glebAccount.Balance}");
+            Console.WriteLine($"{simonAccount.Owner}'s Account balance is : {simonAccount.Balance}");
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
